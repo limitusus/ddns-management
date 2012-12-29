6fb8ea46031ebd195c6d7f00dba2480e5d5dda6f
limitusus <limit.usus@gmail.com>
2012-12-29 22:50:57 +0900

    added column that indicates whether applied or not

--- ddns_init.sql
+++ ddns_init.sql
@@ -2,6 +2,7 @@ CREATE TABLE `ddns` (
       `name` varchar(40) default NULL,
       `type` enum('A','CNAME','MX','NS','SOA') default NULL,
       `data` varchar(40) default NULL,
-      `updatetime` datetime default NULL
+      `updatetime` datetime default NULL,
+      `applied` TINYINT(1) default 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
